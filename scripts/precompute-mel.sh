ac1326c5427f99c6416637a4ff32b2b49490b62f
sdtemple <sethtem@umich.edu>
2026-04-28 19:15:02 -0700

    sbatch precompute

---
+++ scripts/precompute-mel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+table_path=$1
+base_folder=$2
+save_folder=$3
+height=$4
+weight=$5
+papermill \
+    ../notebooks/precompute.ipynb \
+    ../notebooks/precompute-ran.ipynb \
+    -p table_path $table_path \
+    -p base_folder $base_folder \
+    -p save_folder $save_folder \
+    -p height $height \
+    -p width $width
