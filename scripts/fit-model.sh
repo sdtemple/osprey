6ca22f1970f4f391083252b9703ac99d64135ad0
sdtemple <sethtem@umich.edu>
2026-04-29 18:01:26 -0700

    spec fit model

--- scripts/fit-model.sh
+++ scripts/fit-model.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 model=$1
 table=$2
-duration=$3
+num_augments=$3
+num_epochs=$4
+batch_size=$5
+num_workers=$6
+base_folder=$7
+duration=$8
 papermill \
     ../notebooks/fit-$model.ipynb \
     ../notebooks/fit-$model-ran.ipynb \
     -p table_path $table \
     -p output_folder ../results/$model \
-    -p num_augments 3 \
-    -p num_epochs 20 \
-    -p batch_size 256 \
-    -p num_workers 8 \
+    -p num_augments $num_augments \
+    -p num_epochs $num_epochs \
+    -p batch_size $batch_size \
+    -p num_workers $num_workers \
     -p duration $duration \
     -p model_path model.safetensors \
-    -p base_folder /nfs/turbo/umor-sethtem/acoustics-data
+    -p base_folder $base_folder
