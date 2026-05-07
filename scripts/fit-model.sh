b8f4ab391b03e86cfcda2d3d96ddd7b747f4f682
sdtemple <sethtem@umich.edu>
2026-05-07 16:09:37 -0700

    get image size from model

--- scripts/fit-model.sh
+++ scripts/fit-model.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 output=$1
 table=$2
-base_folder=$3
-model=$4
-height=$5
-width=$height
-loss_name=$6
-num_augments=$7
-num_epochs=$8
-batch_size=$9
-num_workers=${10}
-mel_time_size=${11}
+base_folder=/nfs/turbo/umor-sethtem/log-spectrogram-128mel-2048fft-512hop-5dur
+model=$3
+loss_name=$4
+num_augments=$5
+num_epochs=$6
+batch_size=256
+num_workers=$7
+mel_time_size=313
 papermill \
     ../notebooks/fit-spec.ipynb \
     ../notebooks/fit-spec-$model-ran.ipynb \
@@ -22,8 +20,6 @@ papermill \
     -p num_workers $num_workers \
     -p loss_name $loss_name \
     -p model_name $model \
-    -p height $height \
-    -p width $width \
     -p model_path model.safetensors \
-    -p base_folder $base_folder \
+    -p base_folder $base_folder  \
     -p mel_time_size $mel_time_size
